6587313faf27fb321172f084e1b46aba845a4941
Abdul Said <abdulrahman.asaid@gmail.com>
2018-09-08 13:17:20 -0400

    added body parser

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Basic route that sends the user first to the AJAX Page
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'home.html'));
